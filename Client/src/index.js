a309dcaa3cfd22621b5a5cc28ba7f275aaab730c
dhanesh1232 <dhaneshreddy980@gmail.com>
2024-07-04 20:30:02 +0530

    Client Updated

---
+++ Client/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Create a root.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Initial render: Render an element to the root.
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
